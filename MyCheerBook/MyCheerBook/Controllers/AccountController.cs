37244a37f5e1840e8ec4e77cf541fdbd011d2dcf
barrattac@github.com <barrattac@hotmail.com>
2013-12-14 12:45:44 -0500

    Update 13.12.14.1, Layout

---
+++ MyCheerBook/MyCheerBook/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+using MyCheerBook.Models;
+
+namespace MyCheerBook.Controllers
+{
+    
+}
