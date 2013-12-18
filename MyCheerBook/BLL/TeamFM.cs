1e44fb8e8d712af7a867f77a7afb5070b76cb74f
barrattac@github.com <barrattac@hotmail.com>
2013-12-18 12:29:28 -0500

    Added TeamFM

---
+++ MyCheerBook/BLL/TeamFM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BLL
+{
+    public class TeamFM
+    {
+        [Required]
+        [Display(Name = "Team Name")]
+        public string TeamName { get; set; }
+
+        [Display(Name = "Coach Name")]
+        public string Coach { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Address Line 1")]
+        public string Line1 { get; set; }
+
+        [Display(Name = "Address Line 2")]
+        public string Line2 { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public int Zip { get; set; }
+
+        [DataType(DataType.Url)]
+        [Display(Name = "Website Address")]
+        public string Web { get; set; }
+    }
+}
---
+++ MyCheerBook/MyCheerBook/Controllers/TeamController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCheerBook.Controllers
+{
+    public class TeamController : Controller
+    {
+        //
+        // GET: /Team/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreatePage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreatePage()
+        {
+            return View();
+        }
+
+	}
+}
