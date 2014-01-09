eeba74696b8b18e12b604bf3c2edc4fe62103841
barrattac@github.com <barrattac@hotmail.com>
2014-01-09 18:15:19 -0500

    Added a search field

---
+++ MyCheerBook/MyCheerBook/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCheerBook.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        public ActionResult Index()
+        {
+            return PartialView("_Search");
+        }
+        [HttpPost]
+        public ActionResult Search(string word)
+        {
+            return View();
+        }
+	}
+}
