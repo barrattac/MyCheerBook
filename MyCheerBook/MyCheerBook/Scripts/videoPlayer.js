337957a64954325935a3bd7559fc330cb369f3c5
barrattac@github.com <barrattac@hotmail.com>
2014-01-15 13:21:52 -0500

    videos inlarges and plays on click

--- MyCheerBook/MyCheerBook/Scripts/videoPlayer.js
+++ MyCheerBook/MyCheerBook/Scripts/videoPlayer.js
@@ -1,1 +1,22 @@
-﻿
+﻿var movie;
+var playing = false;
+
+$(".images").click(function () {
+    if (playing) {
+        $(".zoom video").get(0).pause();
+        $(movie).css("border", "none");
+        $(".zoom video").addClass("video");
+        $(movie).removeClass("zoom");
+        $(movie).addClass("video");
+        playing = false;
+    }
+    else
+    {
+        movie = this;
+        $(movie).removeClass("images");
+        $(movie).addClass("zoom");
+        $(".zoom video").removeClass("video");
+        $(".zoom video").get(0).play();
+        playing = true;
+    }
+});
